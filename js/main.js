4babe3e0c119a5f62950cde849e3b7bf6c0b3abc
irene macapinlac <irenekaymac@gmail.com>
2021-06-10 13:01:40 +1000

    commenting JS code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const grid = document.querySelector('.grid') //in-built JS method that looks into the HTML for the query when the variable is called
+  let squares = Array.from(document.querySelectorAll('.grid div')) //collect all div's from grid - giving it a specific index number
   const width = 10
+  console.log(squares);
 })
