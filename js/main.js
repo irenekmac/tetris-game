98cb0c4f16cf71eeb5bd0919594149cc965d894c
irene macapinlac <irenekaymac@gmail.com>
2021-06-10 17:27:37 +1000

    js tetriminoes arrays

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid') //in-built JS method that looks into the HTML for the query when the variable is called
   let squares = Array.from(document.querySelectorAll('.grid div')) //collect all div's from grid - giving it a specific index number
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
   const width = 10
-  console.log(squares);
+
+  //Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  
 })
