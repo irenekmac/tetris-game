d7174c43bbc6184caadc31b47403a26af75a6fbf
irene macapinlac <irenekaymac@gmail.com>
2021-06-13 20:56:29 +1000

    gameover function

--- js/main.js
+++ js/main.js
@@ -107,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
       draw()
       displayShape()
       addScore()
+      gameOver()
     }
   }
 
@@ -203,6 +204,14 @@ document.addEventListener('DOMContentLoaded', () => {
         squares = squaresRemoved.concat(squares)
         squares.forEach(cell => grid.appendChild(cell))
       }
+    }
+  }
+
+  // gameover
+  function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      scoreDisplay.innerHTML = 'end'
+      clearInterval(timerId)
     }
   }
 
